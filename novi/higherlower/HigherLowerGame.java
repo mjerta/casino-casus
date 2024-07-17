997e34bd0a89db9c5f0cbccdeaefa9de5bd5cf0d
Maarten Postma <maarten.postma1000@gmail.com>
2024-07-17 14:55:39 +0200

    Refactor and recreate try/catch to a better suit

--- novi/higherlower/HigherLowerGame.java
+++ novi/higherlower/HigherLowerGame.java
@@ -5,9 +5,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HigherLowerGame {
-  private Scanner inputScanner;
-  private boolean gameIsRunning;
-  private Random random;
+  private final Scanner inputScanner;
+  private final Random random;
 
   public HigherLowerGame(Scanner inputScanner) {
     this.inputScanner = inputScanner;
@@ -15,7 +14,7 @@ public class HigherLowerGame {
   }
 
   public void playGame() {
-    gameIsRunning = true;
+    boolean gameIsRunning = true;
 
     // Here you should generate the number to guess
     int randomNumber = random.nextInt(0, 100);
@@ -24,35 +23,25 @@ public class HigherLowerGame {
     while (gameIsRunning) {
       System.out.println(randomNumber);
       System.out.println("Make a guess");
-
-      boolean inputIsSet = false;
-
       try {
-        String string = inputScanner.nextLine();
-        input = Integer.parseInt(string);
-
-      } catch (NumberFormatException e) {
-        System.out.println("Only numbers between 0 and 100");
+        input = inputScanner.nextInt();
+        System.out.printf("You typed: %s\n", input);
+        ammountOfGuesses++;
+        if (input > randomNumber) {
+          System.out.println("That number is too high!");
+        }
+        else if (input < randomNumber) {
+          System.out.println("That number is too low!");
+        }
+        else {
+          System.out.printf("Correct! You guessed the number in %x turns", ammountOfGuesses);
+          // Check if the guess is correct, if so end the game
+          gameIsRunning = false;
+        }
+      } catch (InputMismatchException e) {
+        System.out.println("Only numbers are allowed");
+        inputScanner.nextLine();
       }
-
-      System.out.printf("You typed: %s\n", input);
-      ammountOfGuesses++;
-
-      if (input > randomNumber) {
-        System.out.println("That number is too high!");
-      }
-      else if (input < randomNumber) {
-        System.out.println("That number is too low!");
-      }
-      else {
-        System.out.printf("Correct! You guessed the number in %x turns", ammountOfGuesses);
-        gameIsRunning = false;
-      }
-
-      // Check if the guess is correct, if so end the game
-
-
-
     }
   }
 }
